50597962b044f7ceec57df79a7d2893357b95891
Christiam Mendoza <ch.alb.rz@gmail.com>
2025-11-22 17:14:55 -0500

    Controller complete

--- Controllers/AutoresController.cs
+++ Controllers/AutoresController.cs
@@ -1,10 +1,34 @@
 
+using gestion_biblioteca_api.Data;
+using gestion_biblioteca_api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace gestion_biblioteca_api.Controllers
 {
-    public class AutoresContoller: ControllerBase
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresContoller : ControllerBase
     {
-        
+        private readonly BibliotecaContext _context;
+
+        public AutoresContoller(BibliotecaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Autor>>> GetAutores()
+        {
+            return await _context.Autores.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Autor>> GetAutor(int id)
+        {
+            var autor = await _context.Autores.FirstOrDefaultAsync(a => a.Id == id);
+            return autor == null? NotFound() : autor;
+        }
+
     }
 }
